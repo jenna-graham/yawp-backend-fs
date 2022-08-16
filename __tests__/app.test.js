403c24e293b6e909aa68bc190e119140e7460096
jenna-graham <jclayto2@gmail.com>
2022-08-15 17:59:14 -0700

    pass test for POST route logs in existing user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -21,10 +21,6 @@ describe('backend-express-template routes', () => {
       user: { id: expect.any(String), userName, email },
       message: 'Signed in Successfully',
     });
-    // const response = await request(app)
-    //   .post('/api/v1/users/sessions')
-    //   .send({ email: 'test@jenna.com', password: '12345' });
-    // expect(response.status).toEqual(200);
   });
 
   it('POST, 500 error with email that already exists', async () => {
@@ -32,6 +28,15 @@ describe('backend-express-template routes', () => {
       email: 'test@cutie.com',
     });
     expect(res.status).toBe(500);
+  });
+
+  it('POST logs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(newUser);
+    const res = await request(app).post('/api/v1/users/sessions').send({
+      email: 'test@jenna.com',
+      password: '12345',
+    });
+    expect(res.status).toEqual(200);
   });
   afterAll(() => {
     pool.end();
